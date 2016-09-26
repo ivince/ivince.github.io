32537457f55a6e22a0dbd32ab75ad0ee03d0b437
ivince <ivince.com@gmail.com>
2016-09-26 11:46:24 +0800

    Site updated: 2016-09-26 11:46:21

--- js/instagram.js
+++ js/instagram.js
@@ -6,32 +6,25 @@ var img_h = 0;
 var img_w = 0;
 var view_h;
 var view_w;
-view_h = $(document.body).height();
-view_w = $(document.body).width();
-view_w = view_w > 740 ? 740 : view_w;
 
-/* =============================
- * Disable / Enable Page Scroll
- * when Bootstrap Modals are
- * shown / hidden
- * ============================= */
-
-$('.ins_modal').click(function() {
+$('.ins_modal, .close').click(function() {
     $('.ins_modal').fadeOut(0);
     $('.modal-box img').attr('src', '');
-    //$(".modal_img").off("unveil");
 
     $('body').removeClass('disable-scroll');
 
 });
 
-$('.image-margin').click(function() {
+$('.modal-box').click(function() {
     return false;
 });
 
 $('.ins_img').click(function() {
 
-    $('body').addClass('disable-scroll');
+    var wechatInfo = navigator.userAgent.match(/MicroMessenger\/([\d\.]+)/i) ;
+    if( !wechatInfo ) {
+        $('body').addClass('disable-scroll');
+    }
 
     var url_path = '../ins/standard/';
 
@@ -55,30 +48,23 @@ var cal_img_css = function() {
     view_h = $(".ins_modal").height();
     view_w = $(".ins_modal").width();
     p_w = $('.post').width();
-    console.log("post_w", p_w);
+
     if (view_w > p_w) {
         view_w = p_w;
     }
 
-    console.log("view_w", view_w);
-    console.log("view_h", view_h);
-    console.log("W_H", W_H);
-
     img_h = view_w / W_H;
-    console.log("img_h", img_h);
 
     if ((img_h + 80) > view_h) {
         $('.modal-box').css('height', view_h + 'px');
         $('.modal_img').css('height', view_h - 80 + 'px');
         $('.modal_img').css('width', 'auto');
         $('.modal-box').css('width', (view_h - 80) * W_H + 'px');
-        console.log("大于");
     } else {
         $('.modal-box').css('height', 'auto');
         $('.modal-box').css('width', '100%');
         $('.modal_img').css('width', '100%');
         $('.modal_img').css('height', 'auto');
-        console.log("小于");
     }
 }
 
